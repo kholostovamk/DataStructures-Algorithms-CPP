76752b1ab02c5e65415bad1ca0c5400e2c27e76d
Margo <62659471+kholostovamk@users.noreply.github.com>
2023-10-25 17:50:48 +0000

    united string and number in one input

--- PalindromeCheck.cpp
+++ PalindromeCheck.cpp
@@ -4,70 +4,60 @@
 
 using namespace std;
 
-
 template<typename T> 
-bool isPalindrome (const vector<T>& vector) { //has to be const and reference
+bool isPalindrome(const vector<T>& vec) { //has to be const and reference
     int left = 0;
-    int right = vector.getsize() - 1;
+    int right = vec.size() - 1;
 
-    while (left <right) {
+    while (left < right) {
         //if start and end is not equal then it's not a pallindrome
-         if(vector[left] <vector[right]) {
+        if (vec[left] != vec[right]) {
             return false;
-         }      
-    
-    //increment left and right to check next values
-    left++;
-    right++;
-    
+        }
+        //increment left and right to check next values
+        left++;
+        right--;  // Corrected to decrement right pointer
     }
+    return true;  // Added this line to return true if the loop completes without finding a mismatch
+}
 
-
+bool isNumber(const string& input) {
+    for (char c : input) {
+        if (!isdigit(c)) {
+            return false;  // If any character is not a digit, return false
+        }
+    }
+    return true;  // If all characters are digits, return true
 }
 
 int main() {
+    string input;
 
-//initialize two pointers
-int number;
-string str;
+    //checking a number
+    cout << "Enter a number or a string" <<endl;
+    cin >> input;
 
-//checking a number
-cout << "Enter a number: " <<endl;
-cin >> number;
+    if (isNumber(input)) {
+        vector<int> numVec; //empty vector numVec
+        //while number is not 0
+        for (char c : input) {
+            numVec.push_back(c - '0');  // Convert character to integer
+        }
 
-//convert int into vector out of separate digits
-
-vector<int> numVec; //empty vector numVec
-//while number is not 0
-while (number) {
-    numVec.insert(numVec.begin(), number%10); //at the start of vector inserting last digit
-    number/10;//making sure last number doesn't stay
-}
-
-
-if (isPalindrome(numVec)) {
-    cout << "The number is a palindrome." << endl;
+        if (isPalindrome(numVec)) {
+            cout << "The number is a palindrome." << endl;
+        } else {
+            cout << "The number is not a palindrome." << endl;
+        }
     } else {
-    cout << "The number is not a palindrome." << endl;
+        //coverts string into vector of characters
+        vector<char> charVec(input.begin(), input.end());
+
+        if (isPalindrome(charVec)) {
+            cout << "The string is a palindrome." << endl;
+        } else {
+            cout << "The string is not a palindrome." << endl;
+        }
     }
-
-
-    // For vector of chars:
-    string str;
-    cout << "Enter a string: ";
-    cin >> str;
-
-
-    //coverts string into vector of characters
-    vector<char> charVec(str.begin(), str.end());
-    
-    if (isPalindrome(charVec)) {
-        std::cout << "The string is a palindrome." << std::endl;
-    } else {
-        std::cout << "The string is not a palindrome." << std::endl;
-    }
-
     return 0;
-
-
 }
